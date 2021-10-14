bb7b6a3b51dbb07a1b55f9835e0bb4a01c623793
Opilane TTHK <opilane@tthk.ee>
2021-10-14 09:11:58 +0300

    Week7-Arrays

---
+++ Week7Arrays/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Week7Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab kasutajale hommikusöögi menüüd
+            //programm kuvab kasutajal teha valik
+            //programm kuvab kasutaja valikut konsoolis
+
+            string[] menu = { "Omlett", "pannkoogid", "keedumuna", "neljaviljapuder", "singivõileib"};
+            Console.WriteLine($"Tere, meie menüüs on {menu.Length} valikut.");
+
+            //asendamine:
+            //  menu[4] = "juustuvõileib";
+            //  menu[3] = "kaerapuder";    
+
+            Console.WriteLine("Tänases menüüs on:");
+            for (int i = 0; i < menu.Length; i++)
+            {
+                if(menu[i] == "neljaviljapuder")
+                {
+                    menu[i] = "kaerapuder";
+                }
+                else if (menu[i] == "singivõileib")
+                {
+                    menu[i] = "juustuvõileib";
+                }
+                Console.WriteLine(menu[i]);
+            }
+            Console.WriteLine("Tee oma valikut (sisesta numbrid 1-5):");
+            int userChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            Console.WriteLine($"Olete valinud: {menu[userChoice]}");
+
+
+          /* 
+           * for (int i = 0; i < menu.Length; i++) 
+
+            {
+                Console.WriteLine(menu[i]);
+            }
+
+          */
+          /*
+          foreach(string menuElement in menu)
+          {
+                Console.WriteLine(menuElement);
+          }
+          */
+
+
+
+
+
+
+        }
+    }
+}
